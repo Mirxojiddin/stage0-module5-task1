056fbf29a9fb3c2307b9f27ab9284c9ce6a983ec
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-13 11:10:43 +0500

    javob 3.1

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -125,53 +125,53 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    private static class MergeSort {
-        private static int[] array;
+    // private static class MergeSort {
+    //     private static int[] array;
 
-        public static void mergeSort(int[] arr) {
-            array = arr;
-            int len = array.length;
-            int[] workspace = new int[len];
-            recursiveMergeSort(workspace, 0, len - 1);
-        }
+    //     public static void mergeSort(int[] arr) {
+    //         array = arr;
+    //         int len = array.length;
+    //         int[] workspace = new int[len];
+    //         recursiveMergeSort(workspace, 0, len - 1);
+    //     }
 
-        private static void recursiveMergeSort(int[] workSpace, int lowerBound,
-                                        int upperBound) {
-            if (lowerBound == upperBound) {
-                return;
-            } else {
-                int mid = (lowerBound + upperBound) / 2;
-                recursiveMergeSort(workSpace, lowerBound, mid);
-                recursiveMergeSort(workSpace, mid + 1, upperBound);
-                merge(workSpace, lowerBound, mid + 1, upperBound);
-            }
-        }
+    //     private static void recursiveMergeSort(int[] workSpace, int lowerBound,
+    //                                     int upperBound) {
+    //         if (lowerBound == upperBound) {
+    //             return;
+    //         } else {
+    //             int mid = (lowerBound + upperBound) / 2;
+    //             recursiveMergeSort(workSpace, lowerBound, mid);
+    //             recursiveMergeSort(workSpace, mid + 1, upperBound);
+    //             merge(workSpace, lowerBound, mid + 1, upperBound);
+    //         }
+    //     }
 
-        private static void merge(int[] workspace, int lowPointer,
-                           int highPointer, int upperBound) {
-            int i = 0;
-            int lowerBound = lowPointer;
-            int mid = highPointer - 1;
-            int numberOfItems = upperBound - lowerBound + 1;
+    //     private static void merge(int[] workspace, int lowPointer,
+    //                        int highPointer, int upperBound) {
+    //         int i = 0;
+    //         int lowerBound = lowPointer;
+    //         int mid = highPointer - 1;
+    //         int numberOfItems = upperBound - lowerBound + 1;
 
-            while (lowPointer <= mid && highPointer <= upperBound) {
-                if (array[lowPointer] < array[highPointer]) {
-                    workspace[i++] = array[lowPointer++];
-                } else {
-                    workspace[i++] = array[highPointer++];
-                }
-            }
+    //         while (lowPointer <= mid && highPointer <= upperBound) {
+    //             if (array[lowPointer] < array[highPointer]) {
+    //                 workspace[i++] = array[lowPointer++];
+    //             } else {
+    //                 workspace[i++] = array[highPointer++];
+    //             }
+    //         }
 
-            while (lowPointer <= mid)
-                workspace[i++] = array[lowPointer++];
+    //         while (lowPointer <= mid)
+    //             workspace[i++] = array[lowPointer++];
 
-            while (highPointer <= upperBound)
-                workspace[i++] = array[highPointer++];
+    //         while (highPointer <= upperBound)
+    //             workspace[i++] = array[highPointer++];
 
-            for (i = 0; i < numberOfItems; i++)
-                array[lowerBound + i] = workspace[i];
-        }
-    }
+    //         for (i = 0; i < numberOfItems; i++)
+    //             array[lowerBound + i] = workspace[i];
+    //     }
+    // }
 
     public int[][] sortRaggedArray(int[][] arr) {
         int arrat_len = arr.length;
